a4bab1b45224ec7defd172fa7b30b909f8d570fa
oxananazaruk <132213141+oxananazaruk@users.noreply.github.com>
2023-09-10 21:41:39 +0300

    task 3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,23 @@
+const formEl = document.querySelector('.feedback-form');
+const FEEDBACK_KEY = "feedback-form-state";
 
+formEl.addEventListener('input', onInputHandler);
+
+function onInputHandler(event) {
+    const emailValue = event.currentTarget.elements.email.value;
+    const messageValue = event.currentTarget.elements.message.value;
+
+    const result = {
+       email: emailValue,
+        message: messageValue,
+    };
+
+    localStorage.setItem(FEEDBACK_KEY, JSON.stringify(result))
+
+    updateOutput();
+
+}
+
+function updateOutput() {
+
+}
