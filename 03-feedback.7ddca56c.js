8b272eb521c9066c13a0365306e06dc22d7a42bb
oxananazaruk <132213141+oxananazaruk@users.noreply.github.com>
2023-09-11 14:00:02 +0000

    Deploying to gh-pages from @ oxananazaruk/goit-js-hw-08@0673ed2e08f4a95352cbe85e473c5176d084ef9e 🚀

--- 03-feedback.7ddca56c.js
+++
@@ -1,2 +0,0 @@
-const e=document.querySelector(".feedback-form");e.addEventListener("input",(function(e){const t=e.currentTarget.elements.email.value,a=e.currentTarget.elements.message.value,n={email:t,message:a};localStorage.setItem("feedback-form-state",JSON.stringify(n))}));
-//# sourceMappingURL=03-feedback.7ddca56c.js.map
--- 03-feedback.c1da6557.js
+++
@@ -1,2 +0,0 @@
-!function(){var e=document.querySelector(".feedback-form");e.addEventListener("input",(function(e){var t=e.currentTarget.elements.email.value,a=e.currentTarget.elements.message.value,r={email:t,message:a};localStorage.setItem("feedback-form-state",JSON.stringify(r))}))}();
-//# sourceMappingURL=03-feedback.c1da6557.js.map
