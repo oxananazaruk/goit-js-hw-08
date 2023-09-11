0673ed2e08f4a95352cbe85e473c5176d084ef9e
oxananazaruk <132213141+oxananazaruk@users.noreply.github.com>
2023-09-11 16:59:18 +0300

    task 3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,23 +1,47 @@
+import throttle from 'lodash.throttle';
+
 const formEl = document.querySelector('.feedback-form');
 const FEEDBACK_KEY = "feedback-form-state";
+let parsetData = {};
 
-formEl.addEventListener('input', onInputHandler);
+formEl.addEventListener('input', throttle(onInputHandler, 500));
+formEl.addEventListener('submit', onFormSubmitHandler);
+updateForm();
 
 function onInputHandler(event) {
     const emailValue = event.currentTarget.elements.email.value;
     const messageValue = event.currentTarget.elements.message.value;
 
-    const result = {
+    parsetData = {
        email: emailValue,
-        message: messageValue,
+       message: messageValue,
     };
 
-    localStorage.setItem(FEEDBACK_KEY, JSON.stringify(result))
-
-    updateOutput();
-
+    localStorage.setItem(FEEDBACK_KEY, JSON.stringify(parsetData))
 }
 
-function updateOutput() {
+function onFormSubmitHandler(event) {
+    event.preventDefault();
+    const { email, message } = event.currentTarget.elements;
+    
+    if (email.value === '' || message.value === '') {
+        return alert("All fields must be filed");
+    }
 
+    console.log({ email: email.value, message: message.value });
+
+    event.currentTarget.reset();
+    localStorage.removeItem(FEEDBACK_KEY);
+    parsetData = {};
+}
+
+function updateForm() {
+    const inputData = localStorage.getItem(FEEDBACK_KEY);
+    if (inputData) {
+        parsetData = JSON.parse(inputData);
+       
+        let { email, message } = formEl.elements;
+    email.value = parsetData.email || '';
+    message.value = parsetData.message || '';
+    }
 }
